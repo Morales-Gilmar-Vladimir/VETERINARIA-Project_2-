d86156a3b8528412776cbe65a743cede281b1348
Morales-Gilmar-Vladimir <gilmarmoralesriva2003@gmail.com>
2024-01-15 16:42:06 -0500

    SPRINT 1

--- src/routers/veterinario_routes.js
+++ src/routers/veterinario_routes.js
@@ -1,4 +1,6 @@
 import {Router} from 'express'
+import verificarAutenticacion from '../middlewares/autenticacion.js'
+import { validacionVeterinario } from '../middlewares/validacionVeterinario.js';
 
 const router = Router()
 import {
@@ -15,18 +17,21 @@ import {
     nuevoPassword,
 } from "../controllers/veterinario_controller.js";
 
-router.post("/login", login);
-router.post("/registro", registro);
-router.get("/confirmar/:token", confirmEmail);
-router.get("/veterinarios", listarVeterinarios);
-router.get("/recuperar-password", recuperarPassword);
-router.get("/recuperar-password/:token", comprobarTokenPasword);
-router.post("/nuevo-password/:token", nuevoPassword);
+// RUTAS PUBLICAS
+router.post('/login',login)
+router.post('/registro',registro)
+router.get('/confirmar/:token',confirmEmail)
+router.get('/veterinarios',listarVeterinarios)
+router.get('/recuperar-password',recuperarPassword)
+router.get('/recuperar-password/:token',comprobarTokenPasword)
+router.post('/nuevo-password/:token',nuevoPassword)
 
-router.get("/perfil", perfil);
-router.put('/veterinario/actualizarpassword',actualizarPassword)
-router.get("/veterinario/:id", detalleVeterinario);
-router.put("/veterinario/:id", actualizarPerfil);
+//RUTAS PRIVADAS
+router.get('/perfil',verificarAutenticacion,perfil)
+router.put('/veterinario/actualizarpassword',verificarAutenticacion,actualizarPassword)
+router.get('/veterinario/:id',verificarAutenticacion,detalleVeterinario)
+router.put('/veterinario/:id',verificarAutenticacion,actualizarPerfil)
 
+router.post('/registro',validacionVeterinario,registro)
 
 export default router
